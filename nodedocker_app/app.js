2325062e29dba24890aaad1f3bb3dee4793c0c83
satvickkoneru <konerusatvick@yahoo.com>
2022-08-20 15:18:20 -0400

    world update

--- nodedocker_app/app.js
+++ nodedocker_app/app.js
@@ -3,10 +3,10 @@ const port = process.env.PORT || 8888;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello worl ->>> Blue Vine App!\n'
+  const msg = 'Hello world ->>> Blue Vine App!\n'
   res.end(msg);
 });
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
-});
+});dock
